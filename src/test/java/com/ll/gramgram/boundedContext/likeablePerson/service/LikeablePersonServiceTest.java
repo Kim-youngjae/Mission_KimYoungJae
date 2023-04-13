98151d4e39de040ede87e11c0a89b4bd5617944a
youngjae459 <youngjae459@gmail.com>
2023-04-13 14:21:09 +0900

    feat: likeablePerson service 테스트 추가

---
+++ src/test/java/com/ll/gramgram/boundedContext/likeablePerson/service/LikeablePersonServiceTest.java
@@ -0,0 +1,35 @@
+package com.ll.gramgram.boundedContext.likeablePerson.service;
+
+import com.ll.gramgram.boundedContext.likeablePerson.entity.LikeablePerson;
+import com.ll.gramgram.boundedContext.likeablePerson.repository.LikeablePersonRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@ActiveProfiles("test")
+class LikeablePersonServiceTest {
+    @Autowired
+    LikeablePersonService likeablePersonService;
+    @Autowired
+    LikeablePersonRepository likeablePersonRepository;
+
+    @Test
+    @DisplayName("_ 활용해서 검색하기")
+    void t001() throws Exception {
+        // 좋아하는 대상의 ID가
+        List<LikeablePerson> likeablePeople = likeablePersonRepository.findByToInstaMemberId_username("insta_user4");
+
+        System.out.println("likeablePeople = " + likeablePeople);
+
+        likeablePersonRepository.findByFromInstaMemberIdAndToInstaMemberId_username(2L, "insta_user4");
+    }
+}
